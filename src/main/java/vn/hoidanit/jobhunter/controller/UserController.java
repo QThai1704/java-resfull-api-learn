b1ccbb7e89c858d72773fce1306d64fdbf630146
thaivietis <thainq@vietis.com.vn>
2024-10-23 23:10:42 +0700

    video 37

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -8,12 +8,13 @@ import vn.hoidanit.jobhunter.service.UserService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
@@ -21,33 +22,33 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user")
-    public List<User> fetchAllUser() {
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> fetchAllUser() {
         List<User> userList = this.userService.fetchAllUser();
-        return userList;
+        return ResponseEntity.ok(userList);
     }
 
-    @GetMapping("/user/{id}")
-    public User fetchUserById(@PathVariable("id") long id) {
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> fetchUserById(@PathVariable("id") long id) {
         User user = this.userService.fetchUserById(id);
-        return user;
+        return ResponseEntity.ok(user);
     }
 
-    @PostMapping("/user")
-    public User createNewUser(@RequestBody User postManUser) {
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
         User newUser = this.userService.save(postManUser);
-        return newUser;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
-    @PutMapping("/user")
-    public User putMethodName(@RequestBody User user) {
+    @PutMapping("/users")
+    public ResponseEntity<User> putMethodName(@RequestBody User user) {
         User userUpdate = this.userService.update(user);
-        return userUpdate;
+        return ResponseEntity.ok(userUpdate);
     }
 
-    @DeleteMapping("/user/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
         this.userService.delete(id);
-        return "Xóa thành công";
+        return ResponseEntity.noContent().build();
     }
 }
