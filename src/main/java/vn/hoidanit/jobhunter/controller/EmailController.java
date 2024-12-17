ff6450288ba691feddf3661cf73a7d61b189a591
QThai1704 <quangthai170402@gmail.com>
2024-12-17 10:32:02 +0700

    Video 127: send email

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -3,6 +3,7 @@ package vn.hoidanit.jobhunter.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
 import vn.hoidanit.jobhunter.util.anotation.ApiMessage;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +14,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EmailController {
 
     private final EmailService emailService;
+    private final SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService, SubscriberService subscriberService) {
         this.emailService = emailService;
+        this.subscriberService = subscriberService;
     }
 
     @GetMapping("/emails")
     @ApiMessage("Send simple email")
     public String sendSimpleEmail() {
-        this.emailService.sendSimpleEmail();
-        return new String();
+        // this.emailService.sendEmailSync("quangthai170402@gmail.com", "Test email", "<h1><b>Test email</b></h1>", false, true);
+        this.subscriberService.sendSubscribersEmailJobs();
+        return "ok";
     }
+
 }
